9acd60c48a5800a4f658635969df8ca4d7cbcc0f
LOGA-KRISHNA <logakrishnad@gmail.com>
2025-05-18 14:30:59 +0530

    unwanted1.1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,7 +3,6 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, NgForm } from '@angular/forms';
-import { EmailService } from "./../email.service";
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
@@ -15,14 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class ContactComponent {
 
-  constructor(private emailService: EmailService) { }
-  onSubmit(form: NgForm) {
-    this.emailService.sendEmail(form.value.name,form.value.email,form.value.content);
-    form.reset();
-     // console.log(form.value.email);
-      
-  }
-  title:string="CONTACT";
+ 
 
 
 }
